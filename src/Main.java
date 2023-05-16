38071c15d41e0b6528da55a12dc618bb186fcf00
kouta89 <yamamotojb89@gmail.com>
2023-05-16 13:45:48 +0900

    [add]calendar1.0

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,20 @@ public class Main{
 		//実行時の日付と時刻の情報を取得
 		Calendar ca = Calendar.getInstance();
 		
+		//本日の日付情報
 		int year = ca.get(Calendar.YEAR);
 		int month = ca.get(Calendar.MONTH) + 1;
 		int date = ca.get(Calendar.DATE);
-		System.out.println("本日の日付: "+year+"年"+month+"月"+date+"日"+getDayOfTheWeekNow());
+		String[] weeks= {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
 		
-		//月初めが何曜日だったか取得
-		int dayWeek = ca.get(Calendar.DAY_OF_WEEK);
-		System.out.println(dayWeek);
-		
-		//先週の最終日が何曜日だったかを取得
-		int beforeDay=ca.get(Calendar.DAY_OF_WEEK)-1;
-		System.out.println(beforeDay);
+		System.out.println("本日の日付: "+year+"年"+month+"月"+date+"日"+ getDayOfTheWeekNow());
 		
 		//その月の月末までの日数を取得
 		int daysCount=ca.getActualMaximum(Calendar.DATE);
-		System.out.println(daysCount);
 		
+		//カレンダーに記載しない一日以前の日数を取得
+		ca.set(Calendar.DATE, 1);
+		int beforeBlank=ca.get(Calendar.DAY_OF_WEEK)-1;
 		
 		//色を設定
 		String red    = "\u001b[00;31m";
@@ -33,7 +30,6 @@ public class Main{
 		String[] colors= {red,cyan,yellow,pink};
 		
 		//曜日表示
-		String[] weeks= {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
 		for(int j=0;j < weeks.length;j++) {
 			String colorDays = colors[2] + weeks[j] + end;
 			System.out.printf(" " + colorDays);
@@ -41,22 +37,30 @@ public class Main{
 		System.out.println();
 		
 		//日付表示
-		for(int i=1;i <= beforeDay + daysCount;i++) {
+		for(int i=1;i <= beforeBlank + daysCount;i++) {
 			
-			int days = i - beforeDay;
-			
-			
-			//
-			
+			int days = i - beforeBlank;
 			
 			//daysが0の時表示しない
 			if(days > 0) {
 				
 				//日付が一桁の場合空白を3つ、二桁の場合空白を2つ用意
 				if(days < 10) {
-					System.out.printf("   " + colors[2] + days + end);
+					if(i%7 == 1) {
+						System.out.printf("   " + colors[0] + days + end);
+					}else if(i%7 == 0){
+						System.out.printf("   " + colors[1] + days + end);
+					}else {
+						System.out.printf("   " + days);
+					}
 				}else {
-					System.out.printf("  " + colors[2] + days + end);
+					if((i%7) == 1) {
+						System.out.printf("  " + colors[0] + days + end);
+					}else if((i%7) == 0){
+						System.out.printf("  " + colors[1] + days + end);
+					}else {
+						System.out.printf("  " + days);
+					}
 				}
 				
 			}else {
@@ -87,8 +91,4 @@ public class Main{
 	    throw new IllegalStateException();
 	}
 	
-	//日付文字列から曜日を取得する
-	//public static String getDayOfTheWeek() {
-		
-	//}
 }
